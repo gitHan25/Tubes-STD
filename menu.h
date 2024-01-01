0d5fc94e625faf4b3fd850334f6d7a95db34f4f2
Han25 <farhaneditya03@gmail.com>
2024-01-01 18:01:05 +0700

    add more show feature on menu.cpp

--- menu.h
+++ menu.h
@@ -10,7 +10,9 @@ void hubungkan(list_penulis &LP,List_books LB);
 void deleteP(list_penulis &LP,List_books LB,adrPenulis p);
 void deleteB(List_books &LB,list_penulis &LP,adrBooks p,adrRelation r);
 void show_penulis_with_books(list_penulis LP);
-
+void show_books_with_penulis(List_books LB,list_penulis LP);
+void show_specific_penulis(list_penulis LP,List_books LB);
+void show_specific_book(List_books LB,list_penulis LP);
 
 
 #endif // MENU_H_INCLUDED
